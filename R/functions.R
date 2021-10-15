c0f98689f3382bcc6f6e637af232953ed6c45c42
AarshBatra <aarshbatra.in@gmail.com>
2021-10-16 04:00:00 +0530

    rectify_path and abs_path functions added

---
+++ R/functions.R
@@ -0,0 +1,56 @@
+## libAaRsh package functions--------------------------------------------------
+
+# converting backward slashes to forward slashes in the path names-------------
+
+#' converting backward slashes to forward slashes in the path names
+#'
+#' When we construct relative path names in R using functions like
+#' \code{normalizePath()}, R outputs path names containing "backslashes", but
+#' we require path names containing forward slashes. \code{correct_path} \
+#' function takes in a path name with backward slashes and converts it into a
+#' path name with forward slashes.
+#'
+#' @importFrom stringr str_replace_all
+#' @param backward_slash_path backward slash path
+#' @return \code{forward_slash_path} path with forward slashes
+#'
+#' @examples
+#' correct_path(backward_slash_path = "backward slash path")
+#'
+#' @export
+#'
+
+rectify_path <- function(backward_slash_path){
+  forward_slash_path <- str_replace_all(backward_slash_path, "\\\\", "/")
+  forward_slash_path
+}
+
+# end of function
+
+#======================#
+
+# creating a absolute file path for reading in files
+
+#' This functions creates a absolute file path with forward slashes. It
+#' takes in as its argument a relative path to the data file you want to
+#' read. From that relative path it creates a absolute path to that data
+#' file that is unique to the local file system. This helps avoid file path
+#'errors that people run into when they read each others code.
+#'
+#' @param relPathToDataFile relative path to the data file (using forward slashes)
+#' @return \code{abs_path} absolute path to data file (with forward slashes)
+#'
+#' @examples
+#' abs_path <- function(relPathToDataFile = "path/to/data.csv")
+#'
+#' @export
+
+abs_path <- function(relPathToDataFile){
+  abs_path <- file.path(rectify_path(normalizePath(".")), relPathToDataFile)
+  abs_path
+}
+
+# end of function
+
+
+#=======================#
